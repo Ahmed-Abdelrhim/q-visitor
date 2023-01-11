bb6b5ac31fba0dd93d3cf5bf419b507566608c96
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-01-11 16:27:28 +0200

    update

--- database/migrations/2023_01_11_105039_create_activation_code_table.php
+++ database/migrations/2023_01_11_105039_create_activation_code_table.php
@@ -11,7 +11,7 @@ class CreateActivationCodeTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('activation_code', function (Blueprint $table) {
             $table->id();
@@ -27,7 +27,7 @@ class CreateActivationCodeTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('activation_code');
     }
