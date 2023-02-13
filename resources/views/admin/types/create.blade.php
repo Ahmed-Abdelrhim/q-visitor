48e87508cc4679b01606b7f9e5ebafd37fad9b7d
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-02-13 13:07:01 +0200

    update

--- resources/views/admin/types/create.blade.php
+++ resources/views/admin/types/create.blade.php
@@ -34,7 +34,7 @@
                                     <label>{{ __('Approval Levels') }}</label> <span class="text-danger">*</span>
                                     <select name="level" class="form-control @error('level') is-invalid @enderror">
                                         <option value="0">0</option>
-                                        <option value="1">1</option>
+                                        <option value="1" selected>1</option>
                                         <option value="2">2</option>
                                     </select>
                                     @error('level')
@@ -46,38 +46,40 @@
 
 
                                 <!-- Approval One -->
-                                <div class="form-group">
-                                    <label>{{ __('Approval One') }}</label> <span class="text-danger">*</span>
-                                    <select name="role_one"
-                                            class="form-control @error('role_one') is-invalid @enderror">
-                                        @foreach($roles as $role)
-                                            <option value="{{ $role->id }}">{{ $role->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('role_one')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
+                                @if(isset($roles) && count($roles) > 0)
+                                    <div class="form-group">
+                                        <label>{{ __('Approval One') }}</label> <span class="text-danger">*</span>
+                                        <select name="role_one"
+                                                class="form-control @error('role_one') is-invalid @enderror">
+                                            @foreach($roles as $role)
+                                                <option value="{{ $role->id }}">{{ $role->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('role_one')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
                                     </div>
-                                    @enderror
-                                </div>
 
 
-                                <!-- Approval Two -->
-                                <div class="form-group">
-                                    <label>{{ __('Approval Two') }}</label> <span class="text-danger">*</span>
-                                    <select name="role_two"
-                                            class="form-control @error('role_two') is-invalid @enderror">
-                                        <option value="0" selected>NONE</option>
-                                        @foreach($roles as $role)
-                                            <option value="{{ $role->id }}">{{ $role->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('role_two')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
+                                    <!-- Approval Two -->
+                                    <div class="form-group">
+                                        <label>{{ __('Approval Two') }}</label> <span class="text-danger">*</span>
+                                        <select name="role_two"
+                                                class="form-control @error('role_two') is-invalid @enderror">
+                                            <option value="0" selected>NONE</option>
+                                            @foreach($roles as $role)
+                                                <option value="{{ $role->id }}">{{ $role->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('role_two')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
                                     </div>
-                                    @enderror
-                                </div>
+                                @endif
 
 
                                 <!-- Status -->
