0796ee6bd8831244ec6baa3b40e52436d31e1432
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-05-22 16:28:27 +0200

    update

--- resources/views/admin/visitor/companion.blade.php
+++ resources/views/admin/visitor/companion.blade.php
@@ -17,7 +17,7 @@
                         @can('role_create')
                             <div class="card-header ">
                                 <a href="#" class="btn btn-icon icon-left btn-primary"><i
-                                        class="fas fa-plus"></i> {{ __('files.Add Role') }}</a>
+                                            class="fas fa-plus"></i> {{ __('files.Add Role') }}</a>
                             </div>
                         @endcan
 
@@ -30,11 +30,11 @@
                                         <th>{{ __('files.First Photo') }}</th>
                                         <th>{{ __('files.Second Photo') }}</th>
                                         <th>{{ __('files.Third Photo') }}</th>
-                                        <th>{{ __('files.Fourth Photo') }}</th>
+                                        {{--                                        <th>{{ __('files.Fourth Photo') }}</th>--}}
 
-{{--                                        @if (auth()->user()->can('role_show') || auth()->user()->can('role_edit') || auth()->user()->can('role_delete'))--}}
-{{--                                            <th>{{ __('files.Actions') }}</th>--}}
-{{--                                        @endif--}}
+                                        {{--                                        @if (auth()->user()->can('role_show') || auth()->user()->can('role_edit') || auth()->user()->can('role_delete'))--}}
+                                        {{--                                            <th>{{ __('files.Actions') }}</th>--}}
+                                        {{--                                        @endif--}}
                                     </tr>
                                     </thead>
                                     <tbody>
@@ -43,39 +43,38 @@
                                             <td>{{$visit->visitor->name}}</td>
                                             <td>
                                                 <img
-                                                    src="{{asset('storage/images/'.$visit->reg_no.'/'.$visit->reg_no .'-1.jpg')}}"
-                                                    style="width: 150px; height: 100px;" />
+                                                        src="{{asset('storage/per_images/'.$visit->reg_no.'/'.$visit->reg_no .'.png')}}"
+                                                        style="width: 150px; height: 100px;"/>
                                             </td>
 
 
                                             <td>
                                                 <img
-                                                <img
-                                                    src="{{asset('storage/images/'.$visit->reg_no.'/'.$visit->reg_no .'-2.jpg')}}"
-                                                    style="width: 150px; height: 100px;" />
+                                                        src="{{asset('storage/images/'.$visit->reg_no.'/'.$visit->visitor->national_identification_no .'-1.jpg')}}"
+                                                        style="width: 150px; height: 100px;"/>
                                             </td>
 
                                             <td>
                                                 <img
-                                                    src="{{asset('storage/images/'.$visit->reg_no.'/'.$visit->reg_no .'-3.jpg')}}"
-                                                    style="width: 150px; height: 100px;" />
+                                                        src="{{asset('storage/images/'.$visit->reg_no.'/'.$visit->visitor->national_identification_no .'-2.jpg')}}"
+                                                        style="width: 150px; height: 100px;"/>
                                             </td>
 
-                                            <td>
-                                                <img
-                                                    src="{{asset('storage/images/'.$visit->reg_no.'/'.$visit->reg_no .'-4.jpg')}}"
-                                                    style="width: 150px; height: 100px;" />
-                                            </td>
+                                            {{--                                            <td>--}}
+                                            {{--                                                <img--}}
+                                            {{--                                                        src="{{asset('storage/images/'.$visit->reg_no.'/'.$visit->visitor->national_identification_no .'-4.jpg')}}"--}}
+                                            {{--                                                        style="width: 150px; height: 100px;"/>--}}
+                                            {{--                                            </td>--}}
 
-                                            <td>
+                                            {{--                                            <td>--}}
+                                            {{--                                                <a href="#" class="btn btn-sm btn-icon float-left btn-danger"--}}
+                                            {{--                                                   data-toggle="tooltip" data-placement="top"--}}
+                                            {{--                                                   title="{{__('files.Delete')}}">--}}
+                                            {{--                                                    <i class="fa fa-trash"></i>--}}
+                                            {{--                                                </a>--}}
+                                            {{--                                            </td>--}}
 
-                                                    {{--                                                <a href="#" class="btn btn-sm btn-icon float-left btn-danger"--}}
-                                                    {{--                                                   data-toggle="tooltip" data-placement="top"--}}
-                                                    {{--                                                   title="{{__('files.Delete')}}">--}}
-                                                    {{--                                                    <i class="fa fa-trash"></i>--}}
-                                                    {{--                                                </a>--}}
 
-                                            </td>
                                         </tr>
                                         @foreach($visit->companions as $companion)
                                             <tr class="margin-bottom-2">
@@ -84,49 +83,48 @@
                                                 {{-- $visit->reg_no.'/'.$visit->visitor->national_identification_no  --}}
                                                 <td>
                                                     <img
-                                                        src="{{asset('storage/images/'.$visit->reg_no.'/companions/'. $visit->reg_no .'-' . $companion->id. '-1.jpg')}}"
-                                                        style="width: 150px; height: 100px;"/>
+                                                            src="{{asset('storage/images/'.$visit->reg_no.'/companions/'. $visit->reg_no .'-' . $companion->id. '-1.jpg')}}"
+                                                            style="width: 150px; height: 100px;"/>
                                                 </td>
 
 
                                                 <td>
                                                     <img
-                                                        src="{{asset('storage/images/'.$visit->reg_no.'/companions/'. $visit->reg_no .'-' . $companion->id. '-2.jpg')}}"
-                                                        style="width: 150px; height: 100px;"/>
+                                                            src="{{asset('storage/images/'.$visit->reg_no.'/companions/'. $visit->reg_no .'-' . $companion->id. '-2.jpg')}}"
+                                                            style="width: 150px; height: 100px;"/>
                                                 </td>
 
 
                                                 <td>
                                                     <img
-                                                        src="{{asset('storage/images/'.$visit->reg_no.'/companions/'. $visit->reg_no.'-'  . $companion->id. '-3.jpg')}}"
-                                                        style="width: 150px; height: 100px;"/>
-                                                </td>
-
-
-                                                <td>
-                                                    <img
-                                                        src="{{asset('storage/images/'.$visit->reg_no.'/companions/'. $visit->reg_no .'-' . $companion->id. '-4.jpg')}}"
-                                                        style="width: 150px; height: 100px;"/>
-
+                                                            src="{{asset('storage/images/'.$visit->reg_no.'/companions/'. $visit->reg_no.'-'  . $companion->id. '-3.jpg')}}"
+                                                            style="width: 150px; height: 100px;"/>
                                                 </td>
 
 
 {{--                                                <td>--}}
-
-
-{{--                                                    <a href="#" class="btn btn-sm btn-icon float-left btn-danger"--}}
-{{--                                                       data-toggle="tooltip" data-placement="top"--}}
-{{--                                                       title="{{__('files.Delete')}}" onclick="event.preventDefault();document.getElementById('remove-companion').submit();">--}}
-{{--                                                        <i class="fa fa-trash"></i>--}}
-{{--                                                    </a>--}}
-
-{{--                                                    <form id="remove-companion" class="d-none" method="POST" action="{{route('admin.remove.companion',encrypt($companion->id))}}">--}}
-{{--                                                        @csrf--}}
-{{--                                                    </form>--}}
-
-
+{{--                                                    <img--}}
+{{--                                                            src="{{asset('storage/images/'.$visit->reg_no.'/companions/'. $visit->reg_no .'-' . $companion->id. '-4.jpg')}}"--}}
+{{--                                                            style="width: 150px; height: 100px;"/>--}}
 {{--                                                </td>--}}
-{{--                                                --}}
+
+
+                                                {{--                                                <td>--}}
+
+
+                                                {{--                                                    <a href="#" class="btn btn-sm btn-icon float-left btn-danger"--}}
+                                                {{--                                                       data-toggle="tooltip" data-placement="top"--}}
+                                                {{--                                                       title="{{__('files.Delete')}}" onclick="event.preventDefault();document.getElementById('remove-companion').submit();">--}}
+                                                {{--                                                        <i class="fa fa-trash"></i>--}}
+                                                {{--                                                    </a>--}}
+
+                                                {{--                                                    <form id="remove-companion" class="d-none" method="POST" action="{{route('admin.remove.companion',encrypt($companion->id))}}">--}}
+                                                {{--                                                        @csrf--}}
+                                                {{--                                                    </form>--}}
+
+
+                                                {{--                                                </td>--}}
+                                                {{--                                                --}}
                                             </tr>
                                         @endforeach
                                     @endif
